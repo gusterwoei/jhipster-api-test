d0e5867e8e8eaa7ff8fc8b34a7fb006e2e77df3c
inglab-guster <guster@inglab.com.my>
2019-02-20 15:23:14 +0800

    Initial application generated by JHipster-5.8.1

---
+++ src/main/java/com/gusterwoei/jhipsterapi/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.gusterwoei.jhipsterapi.config.audit;
---
+++ src/main/java/com/gusterwoei/jhipsterapi/repository/search/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data Elasticsearch repositories.
+ */
+package com.gusterwoei.jhipsterapi.repository.search;
